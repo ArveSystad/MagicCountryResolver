06b1d21872b781dd6bc87de8578e8f8242f4f777
Arve Systad <=>
2015-05-12 22:00:19 +0200

    Cleaning, still

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -9,55 +8,39 @@ namespace MagicCountryResolver
     class Program
     {
         private static string[] _countries;
-
+                    
         static void Main(string[] args)
         {
-            _countries = File.ReadAllLines("../countries.txt");
             if(args.Length < 2 ) { 
-                Console.WriteLine("Give me a word and a mode ('none' or 'all')!");
+                Console.WriteLine("Give me a mode ('none' or 'all') and a word to compare against!");
                 Environment.Exit(0);
             }
-            List<string> foundCountries;
 
-            if (args[0] == "none")
-                foundCountries = GetCountriesNotContainingAnyLetters(args[1]).ToList();
-            else if(args[0] == "all")
-                foundCountries = GetCountriesContainingAllLetters(args[1]).ToList();
-            else 
+            Func<string, string, bool> validatorFunction;
+
+            if (args[0].ToLower() == "none")
+                validatorFunction = (word, wordToCompareTo) => !word.HasAnyLetters(wordToCompareTo);
+
+            else if (args[0].ToLower() == "all")
+                validatorFunction = (word, wordToCompareTo) => word.HasAllLetters(wordToCompareTo);
+            else
                 throw new Exception("No mode given, give me one!");
+
+            _countries = File.ReadAllLines("../countries.txt");
+
+            List<string> foundCountries = GetCountries(args[1], validatorFunction).ToList();
 
             foreach (var country in foundCountries)
                 Console.WriteLine(country);
 
             Console.WriteLine();
-
             Console.WriteLine("A total of {0} countries.", foundCountries.Count());
         }
 
-        private static IEnumerable<string> GetCountriesContainingAllLetters(string inThisWord)
+        private static IEnumerable<string> GetCountries(string input, Func<string, string, bool> func)
         {
-            var input = inThisWord.ToLower();
-            foreach (string country in _countries)
-            {
-                var countryToLower = country.ToLower();
-
-                if (countryToLower.HasAllLetters(input))
-                    yield return country;
-            }
-        }
-        
-        private static IEnumerable<string> GetCountriesNotContainingAnyLetters(string inThisWord)
-        {
-            var input = inThisWord.ToLower();
-            foreach (string country in _countries)
-            {               
-                var countryToLower = country.ToLower();
-
-                var hasAnyLetters = countryToLower.HasAnyLetters(inThisWord);
-
-                if (!hasAnyLetters)
-                    yield return country;
-            }
+            input = input.ToLower();
+            return _countries.Where(country => func(country.ToLower(), input));
         }
     }
 }
